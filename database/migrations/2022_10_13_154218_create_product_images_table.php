432c54bae4e98d2d5bd2518beb00254de607a6f6
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-13 11:49:06 -0400

    Renamed `images` table to `product_images`

--- database/migrations/2022_10_13_154218_create_product_images_table.php
+++ database/migrations/2022_10_13_154218_create_product_images_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
             $table->id();
             $table->string("url");
             $table->string("belongs_to_id");
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('product_images');
     }
 };
