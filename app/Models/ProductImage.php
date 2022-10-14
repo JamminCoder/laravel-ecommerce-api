65117a36a4fb1a1c345001ab1fbc0d1fc58b30c8
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-14 11:39:20 -0400

    `Product` and `ProductImage` now have one-to-many realationship

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class ProductImage extends Model
 {
@@ -11,6 +12,9 @@ class ProductImage extends Model
 
     protected $fillable = [
         "url",
-        "belongs_to_id",
     ];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
