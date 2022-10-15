52d233c531f49fc6b5b4643eec551b458cf63c7b
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-15 12:04:43 -0400

    Image upload working

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -7,13 +7,14 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Product;
 use App\Models\ProductImage;
+use Illuminate\Support\Str;
 
 class ProductsController extends Controller
 {
     public static function new(Request $request) {
         $request->validate([
-            "images.*" => "required|mimes:png,jpg,jpeg",
-            "images.*files" => "required|mimes:png,jpg,jpeg",
+            "images.*" => "required|image|mimes:png,jpg,jpeg",
+            "images.*files" => "required|image|mimes:png,jpg,jpeg",
             "catagory" => "required|max:64",
             "name" => "required|unique:products|max:64",
             "description" => "required|max:255",
@@ -27,70 +28,30 @@ class ProductsController extends Controller
         $sku = Product::generateSKU($name);
         $catagory = $request->catagory;
 
-
-        // Configure upload directory and allowed file types
-        $upload_dir = public_path() . "/uploads/";
-        $allowed_types = array('jpg', 'png', 'jpeg');
-        
-        // Checks if user sent an empty form
-        if(!empty(array_filter($_FILES['images']['name']))) {
-    
-            // Loop through each file in files[] array
-            foreach ($_FILES['images']['tmp_name'] as $key => $value) {
-                
-                $file_tmpname = $_FILES['images']['tmp_name'][$key];
-                $file_name = $_FILES['images']['name'][$key];
-                $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
-    
-                // Set upload file path
-                $filepath = $upload_dir.$file_name;
-    
-                // Check file type is allowed or not
-                if(in_array(strtolower($file_ext), $allowed_types)) {
-    
-                    // If file with name already exist then append time in
-                    // front of name of the file to avoid overwriting of file
-                    if(file_exists($filepath)) {
-                        $filepath = $upload_dir.time().$file_name;
-                        
-                        if( move_uploaded_file($file_tmpname, $filepath)) {
-                            echo "{$file_name} successfully uploaded <br />";
-                        }
-                        else {                    
-                            echo "Error uploading {$file_name} <br />";
-                        }
-                    }
-                    else {
-                    
-                        if( move_uploaded_file($file_tmpname, $filepath)) {
-                            echo "{$file_name} successfully uploaded <br />";
-                        }
-                        else {                    
-                            echo "Error uploading {$file_name} <br />";
-                        }
-                    }
-                }
-                else {
-                    
-                    // If file extension not valid
-                    echo "Error uploading {$file_name} ";
-                    echo "({$file_ext} file type is not allowed)<br / >";
-                }
-            }
-        }
-        else {
-            
-            // If no files selected
-            echo "No files selected.";
-        }
+        $uploadedImages = self::uploadImagesFromRequest($request, "images", "product_images");
 
         return [
-            "images" => "Not implemented.",
+            "images" => $uploadedImages,
             "catagory" => $catagory,
             "name" => $name,
             "description" => $description,
             "price" => $price,
             "sku" => $sku,
         ];
+    }
+
+    private static function uploadImagesFromRequest($request, $fieldName, $outputDir) {
+        // Upload code from by https://stackoverflow.com/a/42643349
+        
+        $images = array();
+        if($files = $request->file($fieldName)) {
+            foreach($files as $file){
+                $name = Str::random();
+                $file->move($outputDir, $name);
+                $images[] = $name;
+            }
+        }
+
+        return $images;
     }
 }
