2633219b3b612115da18cb537b36b62475dba6a0
Timothy Batt <jammincoderguy96@gmail.com>
2023-01-10 15:10:51 -0500

    created cabin section migration

---
+++ database/migrations/2023_01_10_200418_create_cabin_section.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cabin_section', function (Blueprint $table) {
+            $table->string("image_path");
+            $table->string("header");
+            $table->string("lead")->nullable();
+            $table->string("link_text")->default("Rent a Cabin");
+            $table->string("href");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cabin_section');
+    }
+};
