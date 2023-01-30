5bd9ecece937af24550cb3f087d42b854eeb6758
Timothy Batt <jammincoderguy96@gmail.com>
2023-01-30 11:26:45 -0500

    Create about page table migration

---
+++ laravel/database/migrations/2023_01_30_160854_create_about_page_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about_page', function (Blueprint $table) {
+            $table->string("header")->default("*About Page Header*");
+            $table->string("body")->default("Enter a description of the website here.");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('about_page');
+    }
+};
